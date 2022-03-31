ba528927c9314f02ef569e024f08858b38c733a4
shoukathalipro <shoukathalipro@gmail.com>
2022-03-31 19:18:46 +0530

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const mongoUrl = "mongodb://localhost:27017/school";
 
+// Create a connection with the database
+// The connection url should contain the db name inside it.
+
 mongoose.connect(mongoUrl, (err, succ) => {
     if (err) {
         console.log(err);
@@ -13,6 +16,8 @@ mongoose.connect(mongoUrl, (err, succ) => {
 
 
 // Step1 : Create a Schema
+// Schema is a predefined structure that we want all of our
+// documents should follow when they are being inserted.
 
 const studentSchema = mongoose.Schema(
     {
@@ -36,10 +41,41 @@ var student1 = new StudentModel(
     }
 )
 
+var student2 = new StudentModel(
+    {
+        name : "Tony Stark",
+        age : 32,
+        email : "tonystark@gmail.com"
+    }
+)
+
+var student3 = new StudentModel(
+    {
+        name : "Steve",
+        age : 32,
+        email : "steve@gmail.com"
+    }
+)
+
+var tempArr = [student2, student3];
+
+
+// .save is only available in mongoose
+// we don't have insertOne function when we're using a model
+
 student1.save( (err, result) => {
     if (err) {
         console.log(err);
     } else {
         console.log(result);
+    }
+})
+
+
+StudentModel.insertMany(tempArr, (err, succ) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Multiple Docs were inserted");
     }
 })
